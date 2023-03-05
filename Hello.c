7a569f4da7f001017491838fc66c4f62da874e8c
akashbansode311 <119726870+akashbansode311@users.noreply.github.com>
2023-03-05 22:46:14 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -5,9 +5,13 @@ int main()
 {
     //Set the Number of Threads i.e. 5
     omp_set_num_threads(5); 
-     //Parallel Region starts threads are spawned
+    //Parallel Region starts threads are spawned
     #pragma omp parallel   
     //Statement Prints Five Times 
     printf("Hello I am Thread %d\n",omp_get_thread_num()); 
     return 0;
 }
+//To run and Compile :
+//ml openmpi-4.1.4-gcc-8.3.0-c7kvqyq
+//$gcc -fopenmp filename.c -o filename
+//$./filename
